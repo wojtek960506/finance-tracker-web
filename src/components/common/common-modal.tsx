7eca52e112af42ec3eeef4baf8811d7ff9b20583
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-11-23 22:37:12 +0100

    add CommonModal * apply it to AddTransactionModal

---
+++ src/components/common/common-modal.tsx
@@ -0,0 +1,50 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import React from "react";
+
+type CommonModalProps = {
+  open: boolean,
+  onOpenChange: (value: boolean) => void;
+  contentClassName?: string;
+  title: string;
+  titleClassName?: string;
+  description: string;
+  descriptionClassName?: string;
+  children: React.ReactNode;
+  trigger?: React.ReactNode;
+}
+
+export const CommonModal = ({
+  open,
+  onOpenChange,
+  contentClassName = "",
+  title,
+  titleClassName = "",
+  description,
+  descriptionClassName = "",
+  children,
+  trigger,
+}: CommonModalProps) => {
+
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      {trigger && <DialogTrigger asChild>{trigger}</DialogTrigger>}
+      <DialogContent className={contentClassName}>
+        <DialogHeader>
+          <DialogTitle className={titleClassName}>{title}</DialogTitle>
+          <DialogDescription className={descriptionClassName}>
+            {description}
+          </DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/components/transaction/modals/add-transaction-modal.tsx
+++ src/components/transaction/modals/add-transaction-modal.tsx
@@ -2,17 +2,10 @@
 
 import { TransactionCreateAPI } from "@/types/transaction-types";
 import { useState } from "react";
-import {
-  Dialog,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogContent,
-  DialogDescription,
-} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { AddTransactionForm } from "./forms";
 import { useTranslation } from "react-i18next";
+import { CommonModal } from "@/components/common/common-modal";
 
 type AddTransactionModalProps = {
   onCreated: (newTxn: TransactionCreateAPI) => Promise<void>;
@@ -22,23 +15,24 @@ export const AddTransactionModal = ({ onCreated }: AddTransactionModalProps) => 
   const { t } = useTranslation("common");
   const [open, setOpen] = useState(false);
   
+  const trigger = (
+    <Button variant="default" className="w-min justify-self-end text-lg">
+      {t('newTransaction')}
+    </Button>
+  );
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="default" className="w-min justify-self-end text-lg">
-          {t('newTransaction')}
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="flex flex-col max-h-[90vh] overflow-auto">
-        <DialogHeader>
-          <DialogTitle>{t('newTransaction')}</DialogTitle>
-          <DialogDescription>{t('newTransactionDescription')}</DialogDescription>
-        </DialogHeader>
-        <AddTransactionForm
-          onOpenChange={setOpen}
-          onCreated={onCreated}
-        />
-      </DialogContent>
-    </Dialog>
+    <CommonModal
+      open={open}
+      onOpenChange={setOpen}
+      title={t('newTransaction')}
+      description={t('newTransactionDescription')}
+      trigger={trigger}
+    >
+      <AddTransactionForm
+        onOpenChange={setOpen}
+        onCreated={onCreated}
+      />
+    </CommonModal>
   )
 }
