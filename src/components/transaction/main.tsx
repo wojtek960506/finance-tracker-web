faaa8ea29276d599c0c1fa306cc77bd3b954ea2e
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-11-27 19:35:02 +0100

    show which filters are applied

--- src/components/transaction/main.tsx
+++ src/components/transaction/main.tsx
@@ -13,6 +13,7 @@ import { Label } from "../ui/label";
 import { TransactionsFilter } from "./transactions-filter";
 import { useTransactionsFilterStore } from "@/store/transactions-filter-store";
 import { useGeneralStore } from "@/store/general-store";
+import { TransactionsFilterHeader } from "./transactions-filter/transactions-filter-header";
 
 export const TransactionsMain = () => {
   const { t } = useTranslation("common");
@@ -27,28 +28,32 @@ export const TransactionsMain = () => {
   return (
     <div className="flex-1 flex flex-col h-full space-y-4 p-6 min-h-[300px]">
       <Card className="overflow-hidden">
-        <CardHeader className="flex justify-between">
-          <CardTitle className="text-2xl">
-            {t('recentTransactions')}
-          </CardTitle>
-          <div className="flex gap-6">
-            <AddTransactionModal
-              onCreated={async (created: TransactionCreateDTO) => {
-                await createMutation.mutateAsync(created);
-              }}
-            />
-            <div className="flex gap-2 justify-self-end items-center border-2 border-black rounded-lg px-4">
-              <Switch
-                id="filter-switch"
-                checked={filterPanelOn}
-                onCheckedChange={(v: boolean) => {
-                  setFilterPanelOn(v);
-                  setIsShown(v);
+        <CardHeader >
+          <div className="flex justify-between">
+            <CardTitle className="text-2xl">
+              {t('recentTransactions')}
+            </CardTitle>
+            <div className="flex gap-6">
+              <AddTransactionModal
+                onCreated={async (created: TransactionCreateDTO) => {
+                  await createMutation.mutateAsync(created);
                 }}
               />
-              <Label htmlFor="filter-switch" className="text-lg">Filter Panel</Label>
+              <div className="flex gap-2 justify-self-end items-center border-2 border-black rounded-lg px-4">
+                <Switch
+                  id="filter-switch"
+                  checked={filterPanelOn}
+                  onCheckedChange={(v: boolean) => {
+                    setFilterPanelOn(v);
+                    setIsShown(v);
+                  }}
+                />
+                <Label htmlFor="filter-switch" className="text-lg">{t('filterPanel')}</Label>
+              </div>
             </div>
+            
           </div>
+          <TransactionsFilterHeader />
         </CardHeader>
         <CardContent className="flex flex-row overflow-hidden">
           {isLoading && <p>Loading...</p>}
---
+++ src/components/transaction/transactions-filter/transactions-filter-header.tsx
@@ -0,0 +1,34 @@
+import { useTransactionsFilterStore } from "@/store/transactions-filter-store"
+import { useTranslation } from "react-i18next";
+
+export const TransactionsFilterHeader = () => {
+  const { t, i18n } = useTranslation("common");
+  const filters = useTransactionsFilterStore(s => s.filters);
+
+  const notShownFilters = ["page", "limit", "sortBy", "sortOrder"];
+
+  const parseValue = (key: string, value: string | number) => {
+    switch (key) {
+      case "startDate":
+      case "endDate":
+        return new Date(value).toLocaleDateString(i18n.language);
+      case "minAmount":
+      case "maxAmount":
+        return value
+      default:
+        return t(`${key}_options.${value as string}`);
+    }
+  }
+
+  return (
+    <ul className="flex gap-2">
+      {Object.entries(filters)
+        .filter(([key, value]) => !notShownFilters.includes(key) && value !== undefined)
+        .map(([key, value]) => (
+          <li key={key} className="border border-2 border-black rounded-lg px-2">
+            {t(key)}: {parseValue(key, value)}
+          </li>
+      ))}
+    </ul>
+  )
+}
