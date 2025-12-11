1ffb1244e7d6b25a3371afecf51021d0379169a3
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-12-11 14:38:09 +0100

    clean statistics-bar-charts * TODO let passing legend

--- src/components/transaction/statistics/statistics-bar-charts/statistics-bar-charts.tsx
+++ src/components/transaction/statistics/statistics-bar-charts/statistics-bar-charts.tsx
@@ -1,10 +1,10 @@
-import { ChartConfig, ChartTooltip } from "@/components/ui/chart";
+import { ChartConfig } from "@/components/ui/chart";
 import { useFormatNumber } from "@/hooks/use-format-number";
 import { cn } from "@/lib/utils";
 import { CommonTransactionStatistics } from "@/types/transaction-types"
 import { useTranslation } from "react-i18next";
-import { CommonBarChart, NonEmptyArray, TooltipFormatterType } from "./common-bar-chart";
-import { ComponentProps, ReactElement } from "react";
+import { CommonBarChart, TooltipFormatterType } from "./common-bar-chart";
+import { ReactElement } from "react";
 import { Bar, Cell } from "recharts";
 
 
@@ -28,15 +28,18 @@ export const TransactionStatisticsBarCharts = ({
   } = statistics;
   const formatNumber = useFormatNumber();
 
-  type TmpType = {
+  type ItemsBarDataType = {
     key: string,
     expense: number,
     income: number,
+  }
+  type AmountBarDataType = ItemsBarDataType & {
     balance: number,
     absBalance: number
-  }[];
-  const dataAmount: TmpType = [];
-  const dataItems: TmpType = [];
+  };
+
+  const dataAmount: AmountBarDataType[] = [];
+  const dataItems: ItemsBarDataType[] = [];
   periodicKeys.forEach(key => {
     const totalAmountExpense = periodicExpense[Number(key)]?.totalAmount ?? 0;
     const totalAmountIncome = periodicIncome[Number(key)]?.totalAmount ?? 0;
@@ -54,25 +57,20 @@ export const TransactionStatisticsBarCharts = ({
       key,
       expense: totalItemsExpense,
       income: totalItemsIncome,
-      balance: totalItemsIncome - totalItemsExpense,
-      absBalance: Math.abs(totalItemsIncome - totalItemsExpense),
     });
   });
 
-  // values in chart legend are based on this, so maybe it will be better
-  // to have some custom component
-  const chartConfig = {
+  const itemsChartConfig = {
     expense: {
-      // label: t('expense'),
       color: "#e57373",
     },
     income: {
-      // label: t('income'),
       color: "#4caf50",
     },
-    // balance: {
-    //   label: t('balance'),
-    // },
+  } satisfies ChartConfig;
+
+  const amountChartConfig = {
+    ...itemsChartConfig,
     balancePositive: {
       label: t('balance'),
       color: "#123456",
@@ -80,17 +78,12 @@ export const TransactionStatisticsBarCharts = ({
     balanceNegative: {
       label: t('balance'),
       color: "#654321",
-    }
-  } satisfies ChartConfig;
+    },
+  } satisfies ChartConfig
 
   const amountTitle = isSum ? "totalAmountTransactions" : "averageAmountTransactions";
   const itemsTitle = isSum ? "totalItemsTransactions" : "averageItemsTransactions";
   
-  const barsOld: NonEmptyArray<{ dataKey: string; fillColor: string }> = [
-    { dataKey: "expense", fillColor: "var(--color-expense)" },
-    { dataKey: "income", fillColor: "var(--color-income)" },
-    { dataKey: "balance", fillColor: "var(--color-balanceNegative)" },
-  ]
 
   const xAxisTickFormatter = (value: string) => (
     (periodicTitle === "month") ? t(`month${value}`) : value
@@ -100,69 +93,68 @@ export const TransactionStatisticsBarCharts = ({
     periodicTitle === "month" ? t(`month${label}`) : label
   )
 
-  function tooltipFormatter (isAmount: boolean) {
-    return function wrapper(value: number, name: string, item: unknown) {
-      const actualName = name === "absBalance" ? "balance" : name;
-      const actualValue = name === "absBalance"
-        ? (item as { payload: { balance: number } }).payload.balance
-        : value
-
-      return (
+  const tooltipItemsFormatter = (value: number, name: string) => {
+    return (
         <div className="flex gap-2 justify-between w-full items-center text-sm">
           <div className="flex gap-2">
             <div className={cn(
               "h-4 w-4 rounded-[5]",
-              name === "expense"
+              name === "expense" 
                 ? "bg-[var(--color-expense)]"
-                : name === "income"
-                  ? "bg-[var(--color-income)]"
-                  : actualValue >= 0
-                    ? "bg-[var(--color-balancePositive)]"
-                    : "bg-[var(--color-balanceNegative)]"
+                : "bg-[var(--color-income)]"
             )} />
-            <span className="font-bold">{t(actualName.toString())}</span>
+            <span className="font-bold">{t(name.toString())}</span>
           </div>
           <span>
-            {`${formatNumber(actualValue.toString(), 2, isAmount)}${isAmount ? ` ${currency}` : ""}`}
+            {`${formatNumber(value.toString(), 2, false)}`}
           </span>
         </div>
       )
-    }
   }
 
+  
+  const tooltipAmountFormatter = (value: number, name: string, item: unknown) => {
+    const actualName = name === "absBalance" ? "balance" : name;
+    const actualValue = name === "absBalance"
+      ? (item as { payload: { balance: number } }).payload.balance
+      : value
 
-
-  // const bars: NonEmptyArray<ReactElement<typeof Bar>> = [
-  const barsAmount: ReactElement<typeof Bar>[] = [
+    return (
+      <div className="flex gap-2 justify-between w-full items-center text-sm">
+        <div className="flex gap-2">
+          <div className={cn(
+            "h-4 w-4 rounded-[5]",
+            name === "expense"
+              ? "bg-[var(--color-expense)]"
+              : name === "income"
+                ? "bg-[var(--color-income)]"
+                : actualValue >= 0
+                  ? "bg-[var(--color-balancePositive)]"
+                  : "bg-[var(--color-balanceNegative)]"
+          )} />
+          <span className="font-bold">{t(actualName.toString())}</span>
+        </div>
+        <span>
+          {`${formatNumber(actualValue.toString(), 2, true)} ${currency}`}
+        </span>
+      </div>
+    )
+  }
+  
+  const barsItems: ReactElement<typeof Bar>[] = [
     <Bar key="1" dataKey="expense" fill="var(--color-expense)" radius={4} />,
     <Bar key="2" dataKey="income" fill="var(--color-income)" radius={4} />,
+  ]
+
+  const barsAmount: ReactElement<typeof Bar>[] = [
+    ...barsItems,
     <Bar
       key="3"
       dataKey="absBalance"
       radius={4}
     >
       {dataAmount.map((row, i) => {
-        const val = row["balance"] as number;
-        return (
-          <Cell
-            key={i}
-            fill={val >= 0 ? "var(--color-balancePositive" : "var(--color-balanceNegative"}
-          />
-        );
-      })}
-    </Bar>
-  ]
-
-  const barsItems: ReactElement<typeof Bar>[] = [
-    <Bar key="1" dataKey="expense" fill="var(--color-expense)" radius={4} />,
-    <Bar key="2" dataKey="income" fill="var(--color-income)" radius={4} />,
-    <Bar
-      key="3"
-      dataKey="absBalance"
-      radius={4}
-    >
-      {dataItems.map((row, i) => {
-        const val = row["balance"] as number;
+        const val = row["balance"];
         return (
           <Cell
             key={i}
@@ -176,24 +168,24 @@ export const TransactionStatisticsBarCharts = ({
   return (
     <div className="flex flex-col">
       <CommonBarChart
-        config={chartConfig}
+        config={amountChartConfig}
         data={dataAmount}
         dataKey="key"
         bars={barsAmount}
         title={t(amountTitle)}
         xAxisTickFormatter={xAxisTickFormatter}
         tooltipLabelFormatter={tooltipLabelFormatter}
-        tooltipFormatter={tooltipFormatter(true) as TooltipFormatterType}
+        tooltipFormatter={tooltipAmountFormatter as TooltipFormatterType}
       />
       <CommonBarChart
-        config={chartConfig}
+        config={itemsChartConfig}
         data={dataItems}
         dataKey="key"
         bars={barsItems}
         title={t(itemsTitle)}
         xAxisTickFormatter={xAxisTickFormatter}
         tooltipLabelFormatter={tooltipLabelFormatter}
-        tooltipFormatter={tooltipFormatter(false) as TooltipFormatterType}
+        tooltipFormatter={tooltipItemsFormatter as TooltipFormatterType}
       />
     </div>
   )
